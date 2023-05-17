8dbc9fa58055ca72d9bf475e8b5153d0cba325c3
Rachel <rachel-barth@live.fr>
2023-05-17 15:48:58 +0200

    Ajustement main-container pour écran plus grand

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,26 @@
-    async function getPhotographers() {
+async function getPhotographers() {
 
-        let data = await fetch('data/photographers.json');
-        let dataJson = await data.json();
-        let photographers = dataJson.photographers;
+    let data = await fetch('data/photographers.json');
+    let dataJson = await data.json();
+    let photographers = dataJson.photographers;
 
-        return ({photographers: [...photographers]})
-    }
+    return(photographers);
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
 
-    async function init() {
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+async function init() {
+    const photographers = await getPhotographers();
+    displayData(photographers);
+};
     
-    init();
+init();
     
